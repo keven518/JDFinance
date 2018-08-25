1a2f33a74c2f44b03f2df635faf5bd3f197d6e7b
KEWEN <kewen@huazhu.com>
2018-08-25 17:22:47 +0800

    '5-16'

--- app/js/public/navbar.vue
+++ app/js/public/navbar.vue
@@ -1,9 +1,43 @@
 <!--  -->
 <template>
-  <div></div>
+  <Panel title="导航条" :class="$style.panel">
+    <ul :class="$style.content">
+      <li>
+        <router-link :to="{ name: 'home' }">
+          <img src="https://img12.360buyimg.com/jrpmobile/jfs/t17650/174/1729132233/4020/2b09fc25/5ad6aac4N620fbed1.png?width=60&height=60" alt="">
+          <p>首页</p>
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="{ name: 'home' }">
+          <img src="https://img12.360buyimg.com/jrpmobile/jfs/t17650/174/1729132233/4020/2b09fc25/5ad6aac4N620fbed1.png?width=60&height=60" alt="">
+          <p>赚钱</p>
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="{ name: 'home' }">
+          <img src="https://img12.360buyimg.com/jrpmobile/jfs/t17650/174/1729132233/4020/2b09fc25/5ad6aac4N620fbed1.png?width=60&height=60" alt="">
+          <p>借钱</p>
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="{ name: 'home' }">
+          <img src="https://img12.360buyimg.com/jrpmobile/jfs/t17650/174/1729132233/4020/2b09fc25/5ad6aac4N620fbed1.png?width=60&height=60" alt="">
+          <p>省钱</p>
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="{ name: 'home' }">
+          <img src="https://img12.360buyimg.com/jrpmobile/jfs/t17650/174/1729132233/4020/2b09fc25/5ad6aac4N620fbed1.png?width=60&height=60" alt="">
+          <p>金融会员</p>
+        </router-link>
+      </li>
+    </ul>
+  </Panel>
 </template>
 
 <script>
+import Panel from '../core/panel.vue'
 export default {
   props: [],
 
@@ -22,9 +56,45 @@ export default {
 
   methods: {},
 
-  components: {}
+  components: {
+    Panel
+  }
 }
 
 </script>
-<style lang='scss' scoped>
+<style lang='scss' module>
+  @import '../../css/element.scss';
+  .panel{
+    @include panel;
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    height: 100px;
+    margin: 0 !important;
+     z-index: 1;
+    h4{
+      display: none;
+    }
+    .content{
+      @include flex(row);
+      justify-content: space-around;
+      li{
+        text-align: center;
+        a{
+          text-decoration: none;
+        }
+        img{
+          width: 44px;
+          height: 44px;
+          display: inline-block;
+          margin: 12px auto 6px;
+        }
+        p{
+          font-size: 22px;
+          color: #656565;
+        }
+      }
+    }
+  }
 </style>
